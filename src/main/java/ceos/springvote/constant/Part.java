d827fb51d73d84f619bd9731ecfec8fafe5a59eb
chlolive-new <otatop5819@gmail.com>
2024-05-29 17:48:40 +0900

    :sparkles: Part와 Position 수정

--- src/main/java/ceos/springvote/constant/Part.java
+++ src/main/java/ceos/springvote/constant/Part.java
@@ -1,5 +1,36 @@
 package ceos.springvote.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum Part {
-    BACKEND, FRONTEND
+    TYPE1(0, "BACKEND"),
+    TYPE2(1, "FRONTEND");
+
+    private Integer idx;
+    private String text;
+
+    Part(Integer idx, String text) {
+        this.idx = idx;
+        this.text = text;
+    }
+
+    public static Part fromText(String text) {
+        for (Part part : Part.values()) {
+            if (part.getText().equals(text)) {
+                return part;
+            }
+        }
+        return null;
+    }
+
+    public static Part fromIdx(Integer idx) {
+        for (Part part : Part.values()) {
+            if (part.getIdx().equals(idx)) {
+                return part;
+            }
+        }
+        return null;
+    }
+
 }
