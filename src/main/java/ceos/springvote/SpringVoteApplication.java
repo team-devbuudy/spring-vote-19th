07ad43ef1e5ee073ac2c04e5ea1adcd839e803b4
hyeongjun im <limbs713@naver.com>
2024-05-29 16:25:14 +0900

    🎉 initial commit

---
+++ src/main/java/ceos/springvote/SpringVoteApplication.java
@@ -0,0 +1,13 @@
+package ceos.springvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ceos/springvote/SpringVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ceos.springvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
