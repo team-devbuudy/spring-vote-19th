de9ebe26936db5452c9970a82a336e65ab956860
hyeongjun im <limbs713@naver.com>
2024-05-29 20:17:23 +0900

    #1 feat: security 설정 파일 추가

---
+++ src/main/java/ceos/springvote/jwt/SecurityConfig.java
@@ -0,0 +1,41 @@
+package ceos.springvote.jwt;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+
+        http.
+                csrf((auth)-> auth.disable());
+        http.
+                formLogin((auth)->auth.disable());
+        http.
+                httpBasic((auth)->auth.disable());
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/vote").permitAll()
+                        .requestMatchers("/login", "/", "/join").hasRole("USER")
+                        .anyRequest().authenticated());
+
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+}
