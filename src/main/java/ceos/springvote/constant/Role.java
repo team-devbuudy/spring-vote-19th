94c3e0c5f24d59d978eddeb2e4945edac62c6411
hyeongjun im <limbs713@naver.com>
2024-05-29 17:54:00 +0900

    :sparkles: Role과 Team Enum 기능 추가

--- src/main/java/ceos/springvote/constant/Role.java
+++ src/main/java/ceos/springvote/constant/Role.java
@@ -1,5 +1,36 @@
 package ceos.springvote.constant;
 
+import lombok.Getter;
+
+@Getter
 public enum Role {
-    USER, GUEST
+    TYPE1(0, "USER"),
+    TYPE2(1, "GUEST");
+
+    private int idx;
+    private String text;
+
+    Role(int idx, String text) {
+        this.idx = idx;
+        this.text = text;
+    }
+
+    public static Role fromText(String text){
+        for(Role role : Role.values()){
+            if(role.getText().equals(text)){
+                return role;
+            }
+        }
+        return null;
+    }
+
+    public static Role fromIdx(int idx) {
+        for (Role role : Role.values()) {
+            if(role.getIdx() == idx){
+                return role;
+            }
+        }
+        return null;
+    }
+
 }
--- src/main/java/ceos/springvote/constant/Team.java
+++ src/main/java/ceos/springvote/constant/Team.java
@@ -4,5 +4,35 @@ import lombok.Getter;
 
 @Getter
 public enum Team {
-    Azito, BeatBuddy, Buldog, Couplelog, TIG
+    TYPE1(0,"Azito"),
+    TYPE2(1,"BeatBuddy"),
+    TYPE3(2,"Buldog"),
+    TYPE4(3,"Couplelog"),
+    TYPE5(4,"TIG");
+
+    private int idx;
+    private String text;
+
+    Team(int idx, String text) {
+        this.idx = idx;
+        this.text = text;
+    }
+
+    public static Team fromText(String text) {
+        for (Team role : Team.values()) {
+            if (role.getText().equals(text)) {
+                return role;
+            }
+        }
+        return null;
+    }
+
+    public static Team fromIdx(int idx) {
+        for (Team role : Team.values()) {
+            if (role.getIdx() == idx) {
+                return role;
+            }
+        }
+        return null;
+    }
 }
