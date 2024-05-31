3fd2176da127d40c8dda1f865c75f87bdf4b8ef7
hyeongjun im <limbs713@naver.com>
2024-06-01 03:45:27 +0900

    #2 :sparkles: Member 회원가입 기능 추가

--- src/main/java/ceos/springvote/domain/Member.java
+++ src/main/java/ceos/springvote/domain/Member.java
@@ -1,27 +1,35 @@
 package ceos.springvote.domain;
 
 import ceos.springvote.constant.Part;
-import ceos.springvote.constant.Position;
 import ceos.springvote.constant.Role;
 import ceos.springvote.constant.Team;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     private String name;
-    private String login_id;
+    @Column(name = "login_id")
+    private String loginId;
     private String password;
     private String email;
     private Part part;
-    private Position position;
     private Role role;
     private Team team;
 }
