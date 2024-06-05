c26f4df3adcc69cba60b45bf1477b90a3c155a1d
chlolive-new <otatop5819@gmail.com>
2024-06-05 15:19:28 +0900

    :white_check_mark: Member 생성자 lombok 추가

--- src/main/java/ceos/springvote/domain/Member.java
+++ src/main/java/ceos/springvote/domain/Member.java
@@ -8,10 +8,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
