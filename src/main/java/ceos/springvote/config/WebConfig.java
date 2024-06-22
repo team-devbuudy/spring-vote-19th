3bc7367615259f91b76b3044d1d6e4636a059406
hyeongjun im <limbs713@naver.com>
2024-06-22 23:30:16 +0900

    :sparkles: cors 설정 파일 추가

---
+++ src/main/java/ceos/springvote/config/WebConfig.java
@@ -0,0 +1,16 @@
+package ceos.springvote.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*"); // 임시로 다 뚫음 실제로는 사용할 건만 남겨놔야함
+    }
+}
